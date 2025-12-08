bb34dddb6c5f72d1f8958efa7f91019d122f1997
l-lavapie071 <l.lavapie071@mybvc.ca>
2025-12-07 20:27:20 -0700

    Added release-build

--- app.config.ts
+++ app.config.ts
@@ -1,40 +1,43 @@
 import { ExpoConfig, ConfigContext } from 'expo/config';
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
-    ...config,
-    name: 'volunteam',
-    slug: 'volunteam',
-    version: '1.0.0',
-    orientation: 'portrait',
-    icon: './assets/icon.png',
-    splash: {
-        image: './assets/splash.png',
-        resizeMode: 'cover',
-        backgroundColor: '#031A62',
-    },
-    updates: {
-        fallbackToCacheTimeout: 0,
-    },
-    assetBundlePatterns: ['**/*'],
-    ios: {
-        supportsTablet: true,
-    },
-    web: {
-        favicon: './assets/favicon.png',
-    },
-    plugins: [
-        [
-            'expo-image-picker',
-            {
-                photosPermission: 'The app accesses your photos to let you add them to events.',
-                cameraPermission: 'The app accesses your camera to let you add pictures to events.',
-            },
-        ],
+  ...config,
+  name: 'volunteam',
+  slug: 'volunteam',
+  version: '1.0.0',
+  orientation: 'portrait',
+  icon: './assets/icon.png',
+  splash: {
+    image: './assets/splash.png',
+    resizeMode: 'cover',
+    backgroundColor: '#031A62',
+  },
+  updates: {
+    fallbackToCacheTimeout: 0,
+  },
+  assetBundlePatterns: ['**/*'],
+  ios: {
+    supportsTablet: true,
+  },
+  android: {
+    package: 'com.don.volunteam', // <-- REQUIRED for Android builds
+  },
+  web: {
+    favicon: './assets/favicon.png',
+  },
+  plugins: [
+    [
+      'expo-image-picker',
+      {
+        photosPermission: 'The app accesses your photos to let you add them to events.',
+        cameraPermission: 'The app accesses your camera to let you add pictures to events.',
+      },
     ],
-    extra: {
-        eas: {
-            projectId: '954f3b8e-1155-4f8f-8601-a2b3126da39e',
-        },
-        IMGBB_API_KEY: process.env.IMGBB_API_KEY,
+  ],
+  extra: {
+    eas: {
+      projectId: '17927353-1970-45a9-8947-e8aaee2a5d70',
     },
+    IMGBB_API_KEY: process.env.IMGBB_API_KEY,
+  },
 });
